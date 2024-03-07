4ab19fd05c88ad55b5b06249a41f643b9c82e883
Angelinamathew <angelinamathew707@gmail.com>
2024-03-07 00:12:59 -0500

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,9 +3,9 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
         StringBuilder str = new StringBuilder();
-        for (int i=1; i<=5;i++) {
-            for (int j=0; j<=5; i++){
-                int result = i*j;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; i++) {
+                int result = i * j;
                 str.append(String.format("%3d |", result));
             }
             str.append("\n");
@@ -15,10 +15,26 @@ public class TableUtilities {
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                int a = i * j;
+                sb.append(String.format("%3d |", a));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j < tableSize; i++) {
+                int b = i * j;
+                sb.append(String.format("%3d |", b));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
